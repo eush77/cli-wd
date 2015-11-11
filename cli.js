ade746bcecdfbb76b68df05507a0e6bd1a67094c
Eugene Sharygin <eush77@gmail.com>
2015-11-11 17:15:57 +0300

    fall back to shell
    This is helpful if what you want to execute is actually a shell function
    or alias.

--- cli.js
+++ cli.js
@@ -17,14 +17,33 @@ try {
   var wd = argv.shift();
   var cmd = argv.shift() || process.env.SHELL;
 
+  try {
+    spawnFrom(wd, cmd, argv);
+  }
+  catch (err) {
+    // Try to execute in shell.
+    argv = ['-c', [cmd].concat(argv).join(' ')];
+    cmd = process.env.SHELL;
+    try {
+      spawnFrom(wd, cmd, argv);
+    }
+    catch (e) {
+      throw err;
+    }
+  }
+}(process.argv.slice(2)));
+
+
+function spawnFrom (wd, cmd, argv) {
   if (kexec) {
     process.chdir(wd);
     kexec(cmd, argv);
   }
   else {
-    spawnSync(cmd, argv, {
+    var err = spawnSync(cmd, argv, {
       cwd: wd,
       stdio: 'inherit'
-    });
+    }).error;
+    if (err) throw err;
   }
-}(process.argv.slice(2)));
+}
