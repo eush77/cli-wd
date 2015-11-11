ccd33cea0072e6764b7b003ab4c49d7d666cc480
Eugene Sharygin <eush77@gmail.com>
2015-11-11 19:04:38 +0300

    print errors on stderr
    Instead of throwing them.

--- cli.js
+++ cli.js
@@ -17,20 +17,33 @@ var shellQuote = require('shell-quote').quote;
   }
 
   var wd = argv.shift();
-  var cmd = argv.shift() || process.env.SHELL;
+  var cmd = argv.shift();
+  if (!cmd) {
+    var startShell = true;
+    cmd = process.env.SHELL;
+  }
 
   try {
+    if (!cmd) throw shellIsUndefined();
     spawnFrom(wd, cmd, argv);
   }
-  catch (err) {
+  catch (err1) {
+    if (startShell) {
+      console.error(err1.toString());
+      process.exit(2);
+    }
+
     // Try to execute in shell.
     argv = ['-c', shellQuote([cmd].concat(argv))];
     cmd = process.env.SHELL;
     try {
+      if (!cmd) throw shellIsUndefined();
       spawnFrom(wd, cmd, argv);
     }
-    catch (e) {
-      throw err;
+    catch (err2) {
+      console.error(err1.toString());
+      console.error(err2.toString(), '(tried to execute in shell)');
+      process.exit(2);
     }
   }
 }(process.argv.slice(2)));
@@ -48,4 +61,9 @@ function spawnFrom (wd, cmd, argv) {
     }).error;
     if (err) throw err;
   }
+}
+
+
+function shellIsUndefined () {
+  return Error('$SHELL is undefined');
 }
